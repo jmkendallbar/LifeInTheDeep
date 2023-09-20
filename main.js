cb99bb9269dde8d7ae3bdb512e04af6b9dbf45de
abhishek <abhishek@coindelta.io>
2023-09-20 19:08:24 +0530

    separate css code

--- main.js
+++ main.js
@@ -14,8 +14,10 @@ import {
 } from "./utils.js";
 import animate from "./animate";
 import drawGraph from "./graph";
+import elementStyle from "./elementStyle";
 // import seal from "./data/Minimized_data.json";
 import seal from "./seal-info/batch_3.json";
+import appendElement from "./appendElement";
 
 // declaring variables
 export let scene, renderer, camera, stats, model;
@@ -48,6 +50,21 @@ let initialSeconds;
 let frequency;
 let minStroke, maxStroke;
 let prevValue;
+// Element Variables
+export let rangeSlider,
+  strokeEle,
+  strokeInnerText,
+  heartEle,
+  heartInnerText,
+  minuteEle,
+  depthEle,
+  stateEle,
+  playSpeedBtn,
+  confirmDuration,
+  pauseBtn,
+  cropBtn,
+  playBtn,
+  resetBtn;
 
 // This is the intializing function when the website will load first
 init();
@@ -173,20 +190,20 @@ function init() {
       // Declaring the varaibles for UI component like button, rangeSlider and more.
       let timer;
       let isTimerStop = true;
-      let rangeSlider = document.createElement("input");
-      let pauseBtn = document.getElementById("pause-icon");
-      let cropBtn = document.getElementById("crop-icon");
-      let playBtn = document.getElementById("play-icon");
-      let resetBtn = document.getElementById("reset-icon");
-      let strokeEle = document.createElement("span");
-      let strokeInnerText = document.createElement("span");
-      let heartEle = document.createElement("span");
-      let heartInnerText = document.createElement("span");
-      let minuteEle = document.createElement("span");
-      let depthEle = document.createElement("span");
-      let stateEle = document.createElement("span");
-      let playSpeedBtn = document.createElement("button");
-      let confirmDuration = document.getElementById("confirmClip");
+      rangeSlider = document.createElement("input");
+      pauseBtn = document.getElementById("pause-icon");
+      cropBtn = document.getElementById("crop-icon");
+      playBtn = document.getElementById("play-icon");
+      resetBtn = document.getElementById("reset-icon");
+      strokeEle = document.createElement("span");
+      strokeInnerText = document.createElement("span");
+      heartEle = document.createElement("span");
+      heartInnerText = document.createElement("span");
+      minuteEle = document.createElement("span");
+      depthEle = document.createElement("span");
+      stateEle = document.createElement("span");
+      playSpeedBtn = document.createElement("button");
+      confirmDuration = document.getElementById("confirmClip");
 
       // timeline crop video's code start from here
       cropBtn.id = "cropBtnId";
@@ -266,57 +283,13 @@ function init() {
       slideOne();
       slideTwo();
 
-      // timeline crop video code --end
-
-      // stroke span
-      strokeEle.textContent = "STROKE RATE";
-      strokeEle.style.position = "absolute";
-      strokeEle.style.bottom = "200px";
-      strokeEle.style.right = "60px";
-      strokeEle.style.color = "#fff";
-      strokeEle.style.fontFamily = "system-ui";
-      strokeEle.style.fontSize = "28px";
-
-      // stroke data
-      strokeInnerText.textContent = "0 spm";
-      strokeInnerText.style.position = "absolute";
-      strokeInnerText.style.bottom = "166px";
-      strokeInnerText.style.right = "60px";
-      strokeInnerText.style.color = "#fff";
-      strokeInnerText.style.fontSize = "32px";
-      strokeInnerText.style.fontFamily = "system-ui";
-
-      // heart span
-      heartEle.textContent = "HEART RATE";
-      heartEle.style.position = "absolute";
-      heartEle.style.bottom = "126px";
-      heartEle.style.right = "60px";
-      heartEle.style.fontSize = "28px";
-      heartEle.style.color = "#fff";
-      heartEle.style.fontFamily = "system-ui";
-
-      // heart data
-      heartInnerText.textContent = "36 bpm";
-      heartInnerText.style.position = "absolute";
-      heartInnerText.style.color = "#fff";
-      heartInnerText.style.bottom = "90px";
-      heartInnerText.style.right = "60px";
-      heartInnerText.style.fontSize = "32px";
-      heartInnerText.style.fontFamily = "system-ui";
+      elementStyle();
 
       // range slider
-      rangeSlider.setAttribute("type", "range");
-      rangeSlider.id = "slider-range";
       rangeSlider.min = Number(sealBehaviourData[0].Seconds) - initialSeconds;
       rangeSlider.max = length;
       rangeSlider.defaultValue =
         Number(sealBehaviourData[0].Seconds) - initialSeconds;
-      rangeSlider.style.position = "absolute";
-      rangeSlider.style.width = "90%";
-      rangeSlider.style.bottom = "60px";
-      rangeSlider.style.left = "60px";
-      rangeSlider.style.height = "20px";
-      rangeSlider.style.cursor = "pointer";
 
       // pause button
       pauseBtn.style.cursor = "pointer";
@@ -348,16 +321,6 @@ function init() {
         rangeSlider.value = rangeSlider.min;
       };
 
-      // confirm video crop btn / function
-      confirmDuration.style.color = "#000";
-      confirmDuration.style.width = "100px";
-      confirmDuration.style.height = "40px";
-      confirmDuration.style.border = "1px solid #fff";
-      confirmDuration.style.borderRadius = "8px";
-      confirmDuration.style.cursor = "pointer";
-      confirmDuration.style.fontWeight = "600";
-      confirmDuration.style.color = "#fff";
-      confirmDuration.style.backgroundColor = "#3264FE";
       confirmDuration.onclick = function () {
         clearInterval(timer);
         if (playBtn.style.display === "block") {
@@ -424,58 +387,6 @@ function init() {
         Plotly.update("chartDiv", updatePlotData.data, updatePlotData.layout);
       };
 
-      // MINUTE ELEMENT
-      minuteEle.textContent = "MINUTES INTO DIVE";
-      minuteEle.style.position = "absolute";
-      minuteEle.style.width = "160px";
-      minuteEle.style.bottom = "110px";
-      minuteEle.style.left = "60px";
-      minuteEle.style.color = "#fff";
-      minuteEle.style.fontSize = "28px";
-      minuteEle.style.fontFamily = "system-ui";
-
-      // DEPTH ELEMENT
-      depthEle.textContent = "DEPTH 196m";
-      depthEle.style.width = "100px";
-      depthEle.style.position = "absolute";
-      depthEle.style.bottom = "400px";
-      depthEle.style.right = "60px";
-      depthEle.style.color = "#fff";
-      depthEle.style.fontSize = "32px";
-      depthEle.style.fontFamily = "system-ui";
-
-      // State ELEMENT
-      stateEle.textContent = "SLEEP STATE ACTIVE WAKING";
-      stateEle.style.width = "170px";
-      stateEle.style.position = "absolute";
-      stateEle.style.textAlign = "center";
-      stateEle.style.top = "238px";
-      stateEle.style.left = "60px";
-      stateEle.style.color = "#fff";
-      stateEle.style.fontSize = "20px";
-      stateEle.style.fontFamily = "system-ui";
-      stateEle.style.backgroundColor = "#4f7942";
-      stateEle.style.borderRadius = "46px";
-      stateEle.style.paddingTop = "16px";
-      stateEle.style.paddingBottom = "16px";
-      stateEle.style.paddingLeft = "30px";
-      stateEle.style.paddingRight = "30px";
-      stateEle.style.border = "4px solid #fff";
-
-      // play speed button
-      playSpeedBtn.type = "button";
-      playSpeedBtn.innerText = "1x";
-      playSpeedBtn.style.position = "absolute";
-      playSpeedBtn.style.width = "60px";
-      playSpeedBtn.style.height = "30px";
-      playSpeedBtn.style.borderRadius = "8px";
-      playSpeedBtn.style.bottom = "16px";
-      playSpeedBtn.style.left = "160px";
-      playSpeedBtn.style.color = "#fff";
-      playSpeedBtn.style.color = "#000";
-      playSpeedBtn.style.cursor = "pointer";
-      playSpeedBtn.style.fontFamily = "system-ui";
-      playSpeedBtn.style.border = "1px solid #fff";
       playSpeedBtn.onclick = function () {
         if (playSpeed === 300) {
           playSpeed = 150;
@@ -510,22 +421,8 @@ function init() {
         }
       };
 
-      // Appending the child into the DOM
-      document.body.appendChild(strokeEle);
-      document.body.appendChild(strokeInnerText);
-      document.body.appendChild(heartEle);
-      document.body.appendChild(heartInnerText);
-      document.body.appendChild(rangeSlider);
-      document.body.appendChild(pauseBtn);
-      document.body.appendChild(playBtn);
-      document.body.appendChild(resetBtn);
-      document.body.appendChild(minuteEle);
-      document.body.appendChild(stateEle);
-      // document.body.appendChild(depthEle);
-      document.body.appendChild(playSpeedBtn);
-      document.body.appendChild(cropBtn);
-      // document.body.appendChild(depthEle);
-      //  End
+      // appending the child element into the DOM
+      appendElement();
 
       // Slider logic start from here
       prevValue = Number(sealBehaviourData[0].Seconds) - initialSeconds;
